b172a093aa89cdd68cbd97d2dded126cc65cdb7a
Paul Hackenberger <paul.hackenberger@gmail.com>
2023-04-21 08:57:04 +0200

    Read readme depending on script path.

--- solar.sh
+++ solar.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-description=$(cat README.md)
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+description=$(cat $SCRIPTPATH/README.md)
 delay=390
 
 while getopts "i:u:p:d:h" opt; do
